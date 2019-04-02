2cb708ef9a8498cd8a7ad7241c7ce122c3361a0d
VitaminOh <dlin01@stuy.edu>
2019-04-02 10:13:38 -0400

    testing initiation

--- Radix.java
+++ Radix.java
@@ -1,17 +1,24 @@
 public class Radix {
+  public static void main(String[] args) {
+    int[] data = new int[10];
+    radixsort(data);
+  }
   @SuppressWarnings("unchecked")
   public static void radixsort(int[]data){
-    MyLinkedList<Integer>[] storage = new MyLinkedList[20];
-    for (int i = 0; i < storage.length; i++) {
-      MyLinkedList nums = new MyLinkedList();
-      storage[i] = nums;
-    }
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    // for (int i = 0; i < buckets.length; i++) {
+    //   MyLinkedList nums = new MyLinkedList();
+    //   buckets[i] = nums;
+    // }
+    MyLinkedList nums = new MyLinkedList();
+    buckets[0] = nums;
+    buckets[0].add(5);
   }
 
-  public static int getMaxDigit(int[] data) {
-    int max = 0;
-    for (int i = 0; i < data.length; i++) {
-
-    }
-  }
+  // public static int getMaxDigit(int[] data) {
+  //   int max = 0;
+  //   for (int i = 0; i < data.length; i++) {
+  //
+  //   }
+  // }
 }
