83f369ef3d5bedc290811b15edf176e398b1a810
Devin Lin <dlin01@stuy.edu>
2019-04-03 22:59:39 -0400

    added a get max digit function

--- Radix.java
+++ Radix.java
@@ -6,19 +6,25 @@ public class Radix {
   @SuppressWarnings("unchecked")
   public static void radixsort(int[]data){
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    // for (int i = 0; i < buckets.length; i++) {
-    //   MyLinkedList nums = new MyLinkedList();
-    //   buckets[i] = nums;
-    // }
-    MyLinkedList nums = new MyLinkedList();
-    buckets[0] = nums;
-    buckets[0].add(5);
+    for (int i = 0; i < buckets.length; i++) {
+      MyLinkedList nums = new MyLinkedList();
+      buckets[i] = nums;
+    }
   }
 
-  // public static int getMaxDigit(int[] data) {
-  //   int max = 0;
-  //   for (int i = 0; i < data.length; i++) {
-  //
-  //   }
-  // }
+  public static int getMaxDigit(int[] data) {
+    int max = 0;
+    for (int num : data) {
+      int count = 1;
+      int temp = num;
+      while (temp / 10 != 0) {
+        count++;
+        temp = temp / 10;
+      }
+      if (count > max) {
+        max = count;
+      }
+    }
+    return max;
+  }
 }
